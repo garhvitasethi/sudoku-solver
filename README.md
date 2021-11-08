8e74e1aa00e413a40ba12e3bca8055382bcc207b
garhvitasethi <82815080+garhvitasethi@users.noreply.github.com>
2021-11-08 14:00:52 +0530

    Sudoku Solver DS Project

---
+++ README.md
@@ -0,0 +1,41 @@
+# UCS757:Building Innovative Systems
+## Submitted by:-
+### Garhvita Sethi
+### 101803708
+### COE 15
+
+# Projects
+
+# Project 3: Sudoku Solver
+## Introduction
+The web app takes an image of unsolved sudoku as the input and shows the solved sudoku
+
+## Requirements
+1. Python
+2. Numpy
+3. Glob
+4. OpenCV-Python
+
+## Live Link
+https://sudoku-solver-ds.herokuapp.com/
+
+## Input Interface
+![Interface](home.jpeg)
+
+## Input
+![Input](sudoku.jpg)
+
+## Output
+![Output](output.jpeg)
+
+## Flowchart to Recognize the Sudoku
+![Flowchart_recognize_sudoku](flowchart3.png) 
+
+## Flowchart to Solve the Sudoku
+![Flowchart_solve_sudoku](flowchart2.png)
+
+## Novelty
+1. Detecting sudoku in an image, can also be extended to detect it in a video stream
+2. Combining Digit recognition with sudoku solving algorithms
+3. Recognizing the numbers already present in the sudoku using KNN algorithm
+4. Solving the sudoku using backtracking
---
+++ knn.py
@@ -0,0 +1,113 @@
+import numpy as np
+import cv2
+import sys
+import os
+
+oldname = sys.argv[1]
+newname = oldname+".jpeg"
+os.rename(oldname,newname)
+
+print(newname)
+
+def findNextCellToFill(grid, i, j):
+    for x in range(i,9):
+        for y in range(j,9):
+            if grid[x][y] == 0:
+                return x,y
+    for x in range(0,9):
+        for y in range(0,9):
+            if grid[x][y] == 0:
+                return x,y
+    return -1,-1
+
+def isValid(grid, i, j, e):
+    rowOk = all([e != grid[i][x] for x in range(9)])
+    if rowOk:
+        columnOk = all([e != grid[x][j] for x in range(9)])
+        if columnOk:
+            # finding the top left x,y co-ordinates of the section containing the i,j cell
+            secTopX, secTopY = 3 *int(i/3), 3 *int(j/3)
+            for x in range(secTopX, secTopX+3):
+                for y in range(secTopY, secTopY+3):
+                    if grid[x][y] == e:
+                        return False
+                return True
+    return False
+
+def solveSudoku(grid, i=0, j=0):
+    i,j = findNextCellToFill(grid, i, j)
+    if i == -1:
+        return True
+    for e in range(1,10):
+        if isValid(grid,i,j,e):
+            grid[i][j] = e
+            if solveSudoku(grid, i, j):
+                return True
+            # Undo the current cell for backtracking
+            grid[i][j] = 0
+    return False
+
+samples = np.load('samples.npy')
+labels = np.load('label.npy')
+
+k = 80
+train_label = labels[:k]
+train_input = samples[:k]
+test_input = samples[k:]
+test_label = labels[k:]
+
+model = cv2.ml.KNearest_create()
+model.train(train_input,cv2.ml.ROW_SAMPLE,train_label)
+
+img = cv2.imread(newname)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(gray,200,255,1)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(5, 5))     
+dilated = cv2.dilate(thresh,kernel)
+ 
+contours, hierarchy = cv2.findContours(dilated,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+boxes = []
+for i in range(len(hierarchy[0])):
+    if hierarchy[0][i][3] == 0:
+        boxes.append(hierarchy[0][i])
+        
+height,width = img.shape[:2]
+box_h = height/9
+box_w = width/9
+number_boxes = []
+soduko = np.zeros((9, 9),np.int32)
+
+for j in range(len(boxes)):
+    if boxes[j][2] != -1:
+        x,y,w,h = cv2.boundingRect(contours[boxes[j][2]])
+        number_boxes.append([x,y,w,h])
+        number_roi = gray[y:y+h, x:x+w]
+        resized_roi=cv2.resize(number_roi,(20,40))
+        thresh1 = cv2.adaptiveThreshold(resized_roi,255,1,1,11,2) 
+        normalized_roi = thresh1/255.  
+        
+        sample1 = normalized_roi.reshape((1,800))
+        sample1 = np.array(sample1,np.float32)
+        
+        retval, results, neigh_resp, dists = model.findNearest(sample1, 1)        
+        number = int(results.ravel()[0])
+        
+        soduko[int(y/box_h)][int(x/box_w)] = number
+               
+
+
+solveSudoku(soduko)
+
+row_sum = map(sum,soduko)
+col_sum = map(sum,zip(*soduko))
+
+for i in range(9):
+    for j in range(9):
+        x = int((i+0.25)*box_w)
+        y = int((j+0.5)*box_h)
+        cv2.putText(img,str(soduko[j][i]),(x,y), 3, 2.5, (0, 0, 255), 2, cv2.LINE_AA)
+cv2.imwrite("public/Sudoku_Solved.jpeg",img)
+
+os.remove(newname)
